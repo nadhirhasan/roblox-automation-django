c27b0088e1164902daf898382ef1f11908c9bb2c
nadhirhasan <hasannadhir2003@gmail.com>
2024-10-30 22:12:20 +0530

    first commit

--- base/forms.py
+++ base/forms.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django import forms
 from .models import SerialKey, Subscription
 
@@ -14,20 +13,3 @@ class SubscriptionForm(forms.ModelForm):
     class Meta:
         model = Subscription
         fields = ['start_date', 'end_date']
-=======
-from django import forms
-from .models import SerialKey, Subscription
-
-class SerialKeyForm(forms.ModelForm):
-    class Meta:
-        model = SerialKey
-        fields = []  # No fields since SerialKey is auto-generated
-
-
-class SubscriptionForm(forms.ModelForm):
-    serial_key = forms.CharField(max_length=100)  # Change this to CharField
-
-    class Meta:
-        model = Subscription
-        fields = ['start_date', 'end_date']
->>>>>>> master
--- base/urls.py
+++ base/urls.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.contrib import admin
 from django.urls import path
 from . import views
@@ -11,17 +10,3 @@ urlpatterns = [
     path('',views.main,name='main')
 ]
 
-=======
-from django.contrib import admin
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('generate-serial/', views.generate_serial_key, name='generate_serial_key'),
-    path('create-subscription/', views.create_subscription, name='create_subscription'),
-    path('verify-serial-key/', views.verify_serial_key, name='verify_serial_key'),
-    path('check-subscription/', views.check_subscription, name='check_subscription'),
-    path('',views.main,name='main')
-]
-
->>>>>>> master
